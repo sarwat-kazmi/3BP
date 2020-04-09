c97fb55e2aecc4fedf0c181702d76f9a0c2b68a5
Sarwat Kazmi <skazmi1@terpmail.umd.edu>
2020-04-09 17:50:22 -0400

    added docstrings for functions 1 and 2

--- spending.py
+++ spending.py
@@ -1,11 +1,35 @@
 class Calculation:
-    """ Docstring for class goes here """
+    """ Calculating various components of and building a user's financial profile """
     
-    function 1 # Sarwat:
-        sdf
+    def __init__(self):
     
-    function 2 # Sarwat:
-        sdf
+    def user_expenses(self):
+        """ Gathers user info on fixed expenses (i.e. rent, tuition, etc.) and variable expenses (i.e. coffee, gas, etc.).
+        
+        Args:
+            total_income (float): total income user makes
+            total_expenses (float): total of expenses user spends on
+            fixed_expenses (float): sum of user fixed expenses
+            var_expenses (float): sum of user variable expenses
+        
+        Returns:
+            CSV of user's financial profile
+        
+        """
+    
+    def spend_check(self):
+        """ Calculates whether user is overspending by checking whether their variable expenses surpass their fixed expenses.
+        
+        Args:
+            break_even (float): point where fixed and variable expenses are equal
+            non_invest (float): amount of non-investment expenditure 
+            ann_int (float): annual interest rate user could earn from investing
+            num_yrs (int): years remaining until retirement
+        
+        Returns:
+            int_earnings (float): interest that could have been earned
+        
+        """
     
     function 3 # Laraib:
         sdf
