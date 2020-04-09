13c003706948688c3431a627335abdf35284e248
Sarwat Kazmi <skazmi1@terpmail.umd.edu>
2020-04-09 12:17:34 -0400

    delete random.py created new files

---
+++ spending.py
@@ -0,0 +1,23 @@
+class Calculation:
+    """ Docstring for class goes here """
+    
+    function 1:
+        sdf
+    
+    function 2:
+        sdf
+    
+    function 3:
+        sdf
+    
+    function 4:
+        sdf
+
+Class Graphs:
+    """ Docstring for class goes here """
+    
+    function 5:
+        sdf
+    
+    function 6:
+        sdf
