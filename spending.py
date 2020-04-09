b1ce0f21a9245f37ba4552f8335e101e4c06e547
laraibz <laraibzia16@gmail.com>
2020-04-09 17:00:48 -0400

    Update spending.py
    added docstrings for my function

--- spending.py
+++ spending.py
@@ -7,11 +7,37 @@ class Calculation:
     function 2 # Sarwat:
         sdf
     
-    function 3 # Laraib:
-        sdf
+    def interest(): # Laraib:
+        """Calculates the amount of interest that will be accumulated on loans
+        as well as remaining balance.
+        
+        Args:
+            principle_amt(float): the principle amount of the loan
+            interest_rate(float): the interest rate on the loan
+            duration(int): the time in years of the loan
+        
+        Returns:
+            (float): the total amount of interest due at the end of the
+            loan period.
+        """
+        # if simple interest
+        #interest_due = (principle_amt * interest_rate * duration) / 100 
+        
+        # if compound interest
+        #interest_due = principle_amt * ((1 + interest_rate/100)**duration)
     
-    function 4 # Laraib:
-        sdf
+    def spending_allocation(): # Laraib:
+        """Calculates the percentages of income that goes towards everyday 
+        expenses (recreation, groceries, dining, etc).
+        
+        Args:
+            total_income(float): total earnings
+            rec_spending(float): the amount spent on recreational activities
+            grocery_spending(float): the amount spent on groceries
+        
+        Returns:
+            (float): the percentage of income going towards each category.
+        """
 
 Class Graphs:
     """ Docstring for class goes here """
