953e79797fa3e3c3d762c437abe4c68ad2412b2a
Sarwat Kazmi <skazmi1@terpmail.umd.edu>
2020-04-10 16:21:01 -0400

    Revert "updated spend_check method"
    This reverts commit b58dbf18a6b87cfdb611516d6925c77ba48eb8e0.

--- spending.py
+++ spending.py
@@ -1,10 +1,5 @@
 class Calculation:
-    """ Calculating various components of and building a user's financial profile
-    
-    total_income (float): total income user makes
-    total_expenses (float): total of expenses user spends on
-    
-    """
+    """ Calculating various components of and building a user's financial profile """
     
     def __init__(self):
     
@@ -12,8 +7,10 @@ class Calculation:
         """ Gathers user info on fixed expenses (i.e. rent, tuition, etc.) and variable expenses (i.e. coffee, gas, etc.).
         
         Args:
-            fixed_expenses (dict): dictionary of user fixed expenses spent on
-            var_expenses (dict): dictionary of user variable expenses spent on
+            total_income (float): total income user makes
+            total_expenses (float): total of expenses user spends on
+            fixed_expenses (float): sum of user fixed expenses
+            var_expenses (float): sum of user variable expenses
         
         Returns:
             CSV of user's financial profile
@@ -24,25 +21,63 @@ class Calculation:
         """ Calculates whether user is overspending by checking whether their variable expenses surpass their fixed expenses.
         
         Args:
-            fixed_spend (float): amount of fixed expenses
-            var_spend (float): amount of variable expenses
+            break_even (float): point where fixed and variable expenses are equal
+            non_invest (float): amount of non-investment expenditure 
+            ann_int (float): annual interest rate user could earn from investing
+            num_yrs (int): years remaining until retirement
         
         Returns:
-            break_even (float): check point to see how expenditures and earnings compare
+            int_earnings (float): interest that could have been earned
         
         """
     
-    function 3 # Laraib:
-        sdf
+    def interest(): # Laraib:
+        """Calculates the amount of interest that will be accumulated on loans
+        as well as remaining balance.
+        
+        Args:
+            principle_amt(float): the principle amount of the loan
+            interest_rate(float): the interest rate on the loan
+            duration(int): the time in years of the loan
+        
+        Returns:
+            (float): the total amount of interest due at the end of the
+            loan period.
+        """
+        # if simple interest
+        #interest_due = (principle_amt * interest_rate * duration) / 100 
+        
+        # if compound interest
+        #interest_due = principle_amt * ((1 + interest_rate/100)**duration)
     
-    function 4 # Laraib:
-        sdf
+    def spending_allocation(): # Laraib:
+        """Calculates the percentages of income that goes towards everyday 
+        expenses (recreation, groceries, dining, etc).
+        
+        Args:
+            total_income(float): total earnings
+            rec_spending(float): the amount spent on recreational activities
+            grocery_spending(float): the amount spent on groceries
+        
+        Returns:
+            (float): the percentage of income going towards each category.
+        """
 
-Class Graphs:
-    """ Docstring for class goes here """
+class Graphs:
+    """  A visual representation of user spending
     
-    function 5 # Miguel:
-        sdf
+    Args:
+        earnings (dict):  dictionary of monthly income
+        expenses (dict):  dictionary of monthly outflow
+    """
     
-    function 6 # Miguel:
-        sdf
+    def __init__(self, earnings, expenses):
+
+    def biggest_expenses(self):
+        """ Provides a visual representation of the users largest expenses
+        """
+        
+    def cutbacks(self):
+        """  Provides a visual representation of how much money the user 
+        would save by cutting back on specific spending habits.
+        """
