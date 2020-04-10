6fecbf718935b560ac20a59293ed1b9ac2c650ea
Miguel R <mruiz99@terpmail.umd.edu>
2020-04-10 16:56:07 -0400

    fixed missing info

--- spending.py
+++ spending.py
@@ -1,9 +1,9 @@
 class Calculation:
     """ Calculating various components of and building a user's financial profile.
     
-    total_income (float): total income user makes
-    total_expenses (float): total of expenses user spends on
-            
+    Attributes:
+        total_income (float): total income user makes
+        total_expenses (float): total of expenses user spends on        
     """
     
     def __init__(self, total_income, total_expenses):
@@ -22,15 +22,17 @@ class Calculation:
         
         """
     
-    def spend_check(self):
-        """ Calculates whether user is overspending by checking whether their variable expenses surpass their fixed expenses.
+    def spend_check(self, fixed_spend, var_spend):
+        """ Calculates whether user is overspending by checking whether their 
+        variable expenses surpass their fixed expenses.
         
         Args:
             fixed_spend (float): amount of fixed expenses
             var_spend (float): amount of variable expenses
         
         Returns:
-            break_even (float): check point to see how expenditures and earnings compare
+            break_even (float): check point to see how expenditures 
+            and earnings compare
         
         """
     
@@ -67,20 +69,30 @@ class Calculation:
         """
 
 class Graphs:
-    """  A visual representation of user spending
+      """  A visual representation of user spending
     
-    Args:
-        earnings (dict):  dictionary of monthly income
-        expenses (dict):  dictionary of monthly outflow
+    Attributes:
+        earnings (dict):  dictionary of yearly income
+        expenses (dict):  dictionary of yearly outflow 
     """
     
     def __init__(self, earnings, expenses):
 
     def biggest_expenses(self):
         """ Provides a visual representation of the users largest expenses
+        
+        Returns:
+            bar plot of largest expenses from expenses dictionary
         """
         
-    def cutbacks(self):
+    def cutbacks(self, habits):
         """  Provides a visual representation of how much money the user 
         would save by cutting back on specific spending habits.
+        
+        Args:
+            habits (dict):  a dictionary of habits that could be cut to save 
+            money, monthly spending
+            
+        Returns:
+            pie chart of money saved  
         """
--- unittest.py
+++ unittest.py
@@ -3,12 +3,24 @@ from spending import Graphs
 
 class TestSkills(unittest.TestCase):
     
-    def setUp(self):
-        self.graph1 = Graphs({"host job": 400, "delivery": 500}, {"cellphone": 100, "gas bill": 60})
-        self.graph2 = Graphs({"cyber security consultant", 7500}, {"mortgage": 1400, "cellphone": 120})
-        
-    def test_biggestexpense(self):
-        self.assertEqual(self.graph2.earnings, dict)
-                             
+     def setUp(self):
+        self.calculation1 = Calculation()
+        self.graph1 = Graphs({"maitre'd": 400.00, "delivery": 500.00}, {"cellphone": 100.00, "gas bill": 60.00})
+        self.graph2 = Graphs({"cyber security consultant", 7500.00}, {"mortgage": 1400.00, "cellphone": 120.00})
+    
+    def test_user_expenses(self):
+        test = self.calculation1.user_expenses(40000.00, 10000.00, 5400.00, 4600.00)
+        self.assertEqual(test, float)
+
+    def test_spend_check(self):        
+    
+    def test_validation(self):
+        with self.assertRaises(ValueError):
+            Graphs(["plumber", 3000], ("welder", 3000))
+    
+    def test_habits(self):
+        lst = {"smoking": 64.00, "eating out for lunch": 200.00}
+        self.assertEqual(self.graph1.cutbacks(lst), list())
+    
     if __name__ == '__main__':
         unittest.main()
