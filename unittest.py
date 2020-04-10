04f565380210bdbe135b3eda796eff66ac5fb259
Miguel R <mruiz99@terpmail.umd.edu>
2020-04-10 18:42:31 -0400

    updated unittest

--- unittest.py
+++ unittest.py
@@ -1,16 +1,16 @@
 import unittest
-from spending import Graphs
+from spending import Calculation, Graphs
 
 class TestSkills(unittest.TestCase):
     
      def setUp(self):
-        self.calculation1 = Calculation()
-        self.graph1 = Graphs({"maitre'd": 400.00, "delivery": 500.00}, {"cellphone": 100.00, "gas bill": 60.00})
+        self.calculation1 = Calculation(40000.00, 10000.00)
+        self.calculation2 = Calculation(98000.00, 33000.00)
+        self.graph1 = Graphs({"maitre'd": 400.00, "delivery driver": 500.00}, {"cellphone": 100.00, "gas bill": 60.00})
         self.graph2 = Graphs({"cyber security consultant", 7500.00}, {"mortgage": 1400.00, "cellphone": 120.00})
     
-    def test_user_expenses(self):
-        test = self.calculation1.user_expenses(40000.00, 10000.00, 5400.00, 4600.00)
-        self.assertEqual(test, float)
+    def test_income(self):
+        self.assertEqual(self.calculation1.
 
     def test_spend_check(self):        
     
