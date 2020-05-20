0ab35c74afe4e78f4d0478d165313f23eca79bf7
laraibz <laraibzia16@gmail.com>
2020-05-20 03:47:57 -0400

    Update spending_test.py

--- spending_test.py
+++ spending_test.py
@@ -1,11 +1,15 @@
 import unittest
+from unittest.mock import patch
 from spending import Calculation, Graphs
 
-class TestSkills(unittest.TestCase):
+class TestSpending(unittest.TestCase):
     """ Test the Calculation methods."""
     def setUp(self):
         
         self.calc1 = Calculation(50000.00) #laraib
+        self.calc1.user_expenses() # laraib 
+        self.calc1.spend_check()
+        self.calc1.spending_allocation()
         
         #self.calculation1 = Calculation(40000.00, 10000.00)
         #self.calculation2 = Calculation(98000.00, 33000.00)
@@ -20,9 +24,12 @@ class TestSkills(unittest.TestCase):
                          " attribute of new Calculation instance is wrong")
     
     def test_income_df(self): #laraib
-        # check that sum of each column in the dataframe is equal to the sum of the dictionary values
-        self.assertEqual(income_df['fixed expenses'].sum, sum(fixed_expenses.values()))
-        self.assertEqual(income_df['variable expenses'].sum, sum(var_expenses.values()))
+        # check that sum of each column in the dataframe is equal to the sum 
+        # of the dictionary values
+        self.assertEqual(income_df['fixed expenses'].sum, 
+                         sum(fixed_expenses.values()))
+        self.assertEqual(income_df['variable expenses'].sum, 
+                         sum(var_expenses.values()))
     
     def test_validation(self):
         with self.assertRaises(ValueError):
@@ -34,19 +41,34 @@ class TestSkills(unittest.TestCase):
         self.assertEqual(self.calculation1.total_income, 40000.00)
         self.assertEqual(self.calculation1.total_expenses, 10000.00)
         self.assertEqual(self.graph3.earnings, {"high school teacher": 35000.00}, {"pepco bill": 720.00})
+    
+    def test_spending_allocation(self): #laraib
+        """ Is spending_allocation correct given fixed_expenses, var_expenses 
+        dictionaries as well as total_income? """
+        self.assertEqual(spending_allocation(self.calc1))
+    
+    #fixed_expenses = {"cellphone": 1200.00, "gas": 720.00, "car": 200.00}
+    #var_expenses = {"movies": 20.00, "groceries": 65.00}
+    #total_income = 50000.00
+    
+    #@patch('builtins.input', side_effect = [total_income, fixed_expenses, 
+                                            #var_expenses])
+    #def test_spendalloc(self, mock_inputs):
+        #pctg1, pctg2 = spending_allocation()
+        #self.assertEqual(pctg1, pctg2, 4.24, 0.17) 
         
     def test_habits(self):
         dict1 = {"smoking": 64.00, "eating out for lunch": 200.00}
         self.assertEqual(self.graph1.cutbacks(dict1), {"smoking": 64.00, "eating out for lunch": 200.00}) 
 
-class TestMetroSystem(unittest.TestCase): #laraib
+class TestGraphs(unittest.TestCase): #laraib
     """ Test Graphs methods."""
     def setUp(self):
         
         self.graph1 = Graphs()
         self.graph1.expenses({"cellphone": 1200.00, "gas": 720.00, "car": 200.00},
                              {"movies": 20.00, "groceries": 65.00})
-        self.graph1.cutbacks(50000.00)
+        self.graph1.habits(50000.00)
     
     def test_expenses(self): #laraib
         """ self.expense is a dictionary created from the 
@@ -59,6 +81,9 @@ class TestMetroSystem(unittest.TestCase): #laraib
         
         #self.graph1 = Graphs({"maitre'd": 19200.00, "delivery driver": 24000.00}, {"cellphone": 1200.00, "gas bill": 720.00})
         #self.graph2 = Graphs({"cyber security consultant", 90000.00}, {"mortgage": 16800.00, "cellphone": 1440.00})
+    
+    def test_habits(self): 
+        
     
 if __name__ == '__main__':
     unittest.main()
